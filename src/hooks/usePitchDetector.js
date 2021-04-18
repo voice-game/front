345290511c61ecc276c17b84bc5addedceb428cf
KimJaedeok <64330699+jdcoder27@users.noreply.github.com>
2021-04-18 16:30:33 +0900

    [JD] ADD: description of usePitchDetector

--- src/hooks/usePitchDetector.js
+++ src/hooks/usePitchDetector.js
@@ -3,6 +3,16 @@ import ml5 from "ml5";
 import getAudioContext from "../utils/getAudioContext";
 import getMedia from "../utils/getMedia";
 
+/**
+ *
+ * @param {boolean} useAudio state that manages audio on/off
+ * @returns Ref of pitchDetector
+ *
+ * You can get the pitch value through the method of pitchDetector, getPitch().
+ * The getPitch method returns a promise.
+ *
+ * reference https://ml5js.org/reference/api-PitchDetection/
+ */
 const usePitchDetector = (useAudio) => {
   const pitchDetectorRef = useRef(null);
 
@@ -21,6 +31,7 @@ const usePitchDetector = (useAudio) => {
       })();
     }
   }, [useAudio]);
+
   return pitchDetectorRef.curret;
 };
 
