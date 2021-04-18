b1f2d2c98b23acb73f6519cbdf9d673e48c91c67
KimJaedeok <64330699+jdcoder27@users.noreply.github.com>
2021-04-18 16:06:39 +0900

    [JD] ADD: usePitchDetector

--- src/components/RoadRoller/RoadRoller.jsx
+++ src/components/RoadRoller/RoadRoller.jsx
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import Game from "../../games/roadRoller";
-import useAudioContext from "../../hooks/useAudioContext";
 import useCanvas from "../../hooks/useCanvas";
-import useMediaStream from "../../hooks/useMediaStream";
+import usePitchDetector from "../../hooks/usePitchDetector";
 import GameFrame from "../GameFrame/GameFrame";
 import GameOption from "../GameOption/GameOption";
 import GameResult from "../GameResult/GameResult";
 
 const RoadRoller = (props) => {
-  const [useAudio, setUseAudio] = useState(true);
-  const audioContext = useAudioContext(useAudio, { sampleRate: 12000 });
-  const mediaStream = useMediaStream(useAudio, { audio: true, video: false });
+  const [useAudio, setUseAudio] = useState(false);
+  const pitchDetector = usePitchDetector(useAudio);
   const game = useCanvas(Game);
 
   return (
@@ -18,6 +16,7 @@ const RoadRoller = (props) => {
       <GameOption />
       <GameFrame canvasRef={game} />
       <GameResult />
+      <button onClick={() => setUseAudio(true)}>click</button>
     </div>
   );
 };
