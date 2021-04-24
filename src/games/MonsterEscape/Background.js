a414a1d940f1fcacc0bb3fce2087d9c759f9e8ae
kwon, minho <minhob38@gmail.com>
2021-04-24 18:35:05 +0900

    [MH] Refactor Background class

--- src/games/MonsterEscape/Background.js
+++ src/games/MonsterEscape/Background.js
@@ -1,11 +1,22 @@
-function Background(canvasWidth, canvasHeight, images) {
-  this.canvasWidth = canvasWidth;
-  this.canvasHeight = canvasHeight;
-  this.images = images;
+class Background{
+  constructor(canvasWidth, canvasHeight, images) {
+    this.canvasWidth = canvasWidth;
+    this.canvasHeight = canvasHeight;
+    this.images = images;
+  }
+
+  animate = function (ctx) {
+    ctx.drawImage(
+      this.images[0],
+      0,
+      100,
+      263,
+      263,
+      0,
+      0,
+      this.canvasWidth,
+      this.canvasHeight
+    );
+  };
 }
-
-Background.prototype.animate = function (ctx) {
-  ctx.drawImage(this.images[0], 0, 100, 263, 263, 0, 0, this.canvasWidth, this.canvasHeight);
-};
-
 export default Background;
