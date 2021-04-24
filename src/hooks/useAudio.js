2f153d716b9bb56aa784cd378ea527ad414e3f62
KimJaedeok <64330699+jdcoder27@users.noreply.github.com>
2021-04-25 01:28:27 +0900

    [JD] ADD: useAudio

---
+++ src/hooks/useAudio.js
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from "react";
+import getAudioContext from "../utils/getAudioContext";
+import getMedia from "../utils/getMedia";
+
+/**
+ *
+ * @param {object} contextOption option for audioContext
+ * @param {object} mediaOption option for mediaStream
+ * @returns audioState, audioContext Ref, mediaStream Ref
+ */
+const useAudio = (contextOption, mediaOption) => {
+  const [isAudioUse, setIsAudioUse] = useState(false);
+  const audioContextRef = useRef(null);
+  const micStreamRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setTimeout(() => {
+          audioContextRef.current = getAudioContext(contextOption);
+          setIsAudioUse(true);
+        }, 200);
+
+        micStreamRef.current = await getMedia(mediaOption);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, [contextOption, mediaOption]);
+
+  return {
+    isAudioUse,
+    audioContextRef,
+    micStreamRef,
+  };
+};
+
+export default useAudio;
