5696eaed3e033b3b416ecd5fcc1cfc1420764311
seongjuseo <canwefly89@gmail.com>
2021-04-30 00:51:17 +0900

    [SJ] after reset --hard

--- src/components/LittleForestContainer/LittleForestContainer.jsx
+++ src/components/LittleForestContainer/LittleForestContainer.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
 
+import Game from "../../games/littleForest";
+import BackGround from "../../games/littleForest/Background";
+import GameMap from "../../games/littleForest/GameMap";
+import GameManual from "../GameManual/GameManual";
+import Loading from "../Loading/Loading";
+
 import useAudio from "../../hooks/useAudio";
 import useCanvas from "../../hooks/useCanvas";
 import usePitchDetector from "../../hooks/usePitchDetector";
@@ -7,14 +13,8 @@ import useLoadedImage from "../../hooks/useLoadedImage";
 
 import Canvas from "../shared/Canvas/Canvas";
 
-import Game from "../../games/littleForest";
-import BackGround from "../../games/littleForest/Background";
-import GameMap from "../../games/littleForest/GameMap";
-
-import pickRandom from "../../utils/pickRandom";
-import GameManual from "../GameManual/GameManual";
-
 import manualImage from "../../images/manuals/manual_littleForest.png";
+import backgroundImages from "../../games/images/littleForest/backgroundImages";
 
 const LittleForestContainer = () => {
   const { image, isLoaded } = useLoadedImage("littleForest");
@@ -22,7 +22,7 @@ const LittleForestContainer = () => {
   const pitchDetectorRef = usePitchDetector(
     useAudio({ samplerate: 12000 }, { audio: true, video: false })
   );
-
+console.log(image)
   const TILE_SIZE = 32;
   const WIDTH = TILE_SIZE * 43;
   const HEIGHT = TILE_SIZE * 19;
@@ -55,29 +55,31 @@ const LittleForestContainer = () => {
     isLoaded
   );
 
-  if (!isLoaded) {
-    return null;
-  }
-
   return (
     <>
-      <GameManual imgSrc={manualImage} />
-      <Canvas
-        id="game-layer"
-        ref={game}
-        position="absolute"
-        width={WIDTH}
-        height={HEIGHT}
-      />
-      <Canvas
-        id="background-layer"
-        ref={background}
-        position="absolute"
-        width={WIDTH}
-        height={HEIGHT}
-        bgImage={pickRandom(image.backgrounds).src}
-      />
-      <div>W: 점프 A: 좌 D: 우</div>
+      {isLoaded ? (
+        <>
+          <GameManual imgSrc={manualImage} />
+          <Canvas
+            id="game-layer"
+            ref={game}
+            position="absolute"
+            width={WIDTH}
+            height={HEIGHT}
+          />
+          <Canvas
+            id="background-layer"
+            ref={background}
+            position="absolute"
+            width={WIDTH}
+            height={HEIGHT}
+            bgImage={backgroundImages[currentMap]}
+          />
+          <div>W: 점프 A: 좌 D: 우</div>
+        </>
+      ) : (
+        <Loading />
+      )}
     </>
   );
 };
