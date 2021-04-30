d42be587d64392886830fc49ec44ece66545c7a5
kwon, minho <minhob38@gmail.com>
2021-04-30 14:59:22 +0900

    [MH] Convert mountain constructor to class

--- src/games/monsterEscape/Mountain.js
+++ src/games/monsterEscape/Mountain.js
@@ -1,72 +1,75 @@
-function Mountain(peakNum, width, height, color) {
-  this.peakNum = peakNum;
-  this.width = width;
-  this.height = height;
-  this.color = color;
-  this.points = [];
-  this.peakGap = 0;
-}
-
-Mountain.prototype.getPeakHeight = function () {
-  const minHeight = this.height / 20;
-  const deviation = (this.height - minHeight) / 10;
-
-  return minHeight + Math.random() * deviation;
-};
-
-Mountain.prototype.setPeakPoint = function () {
-  this.peakGap = this.width / (this.peakNum - 1);
-
-  for (let i = 0; i < this.peakNum; i++) {
-    this.points[i] = {
-      x: i * this.peakGap,
-      y: this.height - this.getPeakHeight(),
-    };
-  }
-};
-
-Mountain.prototype.animate = function (ctx, speed) {
-  this.points.forEach((point) => (point.x -= speed));
-
-  const startX = this.points[this.points.length - 1].x;
-  const endX = this.points[0].x;
-
-  if (startX <= this.width + this.peakGap) {
-    this.points.push({
-      x: this.width + 2 * this.peakGap,
-      y: this.height - this.getPeakHeight(),
-    });
-  } else if (endX <= -this.width) {
-    this.points.shift();
+class Mountain {
+  constructor(peakNum, width, height, color) {
+    this.peakNum = peakNum;
+    this.width = width;
+    this.height = height;
+    this.color = color;
   }
 
-  ctx.beginPath();
+  points = [];
+  peakGap = 0;
 
-  let currentX = this.points[0].x;
-  let currentY = this.points[0].y;
+  getPeakHeight() {
+    const minHeight = this.height / 20;
+    const deviation = (this.height - minHeight) / 10;
 
-  ctx.moveTo(currentX, currentY);
+    return minHeight + Math.random() * deviation;
+  };
 
-  for (let i = 1; i < this.points.length; i++) {
-    const nextX = this.points[i].x;
-    const nextY = this.points[i].y;
-    const midX = (currentX + nextX) / 2;
-    const midY = (currentY + nextY) / 2;
+  setPeakPoint() {
+    this.peakGap = this.width / (this.peakNum - 1);
 
-    ctx.quadraticCurveTo(currentX, currentY, midX, midY);
+    for (let i = 0; i < this.peakNum; i++) {
+      this.points[i] = {
+        x: i * this.peakGap,
+        y: this.height - this.getPeakHeight(),
+      };
+    }
+  };
 
-    currentX = this.points[i].x;
-    currentY = this.points[i].y;
+  animate(ctx, speed) {
+    this.points.forEach((point) => (point.x -= speed));
+
+    const startX = this.points[this.points.length - 1].x;
+    const endX = this.points[0].x;
+
+    if (startX <= this.width + this.peakGap) {
+      this.points.push({
+        x: this.width + 2 * this.peakGap,
+        y: this.height - this.getPeakHeight(),
+      });
+    } else if (endX <= -this.width) {
+      this.points.shift();
+    }
+
+    ctx.beginPath();
+
+    let currentX = this.points[0].x;
+    let currentY = this.points[0].y;
+
+    ctx.moveTo(currentX, currentY);
+
+    for (let i = 1; i < this.points.length; i++) {
+      const nextX = this.points[i].x;
+      const nextY = this.points[i].y;
+      const midX = (currentX + nextX) / 2;
+      const midY = (currentY + nextY) / 2;
+
+      ctx.quadraticCurveTo(currentX, currentY, midX, midY);
+
+      currentX = this.points[i].x;
+      currentY = this.points[i].y;
+    }
+
+    ctx.lineTo(currentX, currentY);
+    ctx.lineTo(this.width, this.height);
+    ctx.lineTo(this.points[0].x, this.height);
+
+    ctx.fillStyle = this.color;
+    ctx.fill();
+
+    ctx.closePath();
   }
-
-  ctx.lineTo(currentX, currentY);
-  ctx.lineTo(this.width, this.height);
-  ctx.lineTo(this.points[0].x, this.height);
-
-  ctx.fillStyle = this.color;
-  ctx.fill();
-
-  ctx.closePath();
 };
 
 export default Mountain;
