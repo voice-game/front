12400a2a325145b783edf4c5d7147807741b35ed
KimJaedeok <jdcoder27@gmail.com>
2021-05-02 18:20:53 +0900

    [JD] FIX: error

--- src/hooks/useRandomCharacter.js
+++ src/hooks/useRandomCharacter.js
@@ -5,7 +5,9 @@ import { useState } from "react";
  * @param {number} listLength length of characterList
  * @returns ramdom index under listLength
  */
-const useRandomCharacter = (listLength) => {
+const useRandomCharacter = (image) => {
+  const listLength = image.characters ? image.characters.length : 0;
+
   const randomIndex = Math.random() * listLength;
   const floored = Math.floor(randomIndex);
   const [characterIndex, setCharacterIndex] = useState(floored);
